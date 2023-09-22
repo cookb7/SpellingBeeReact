4306d33907c77049efcc850783b46500f1869ad4
cookb7 <cookb7@oregonstate.edu>
2023-09-22 11:16:10 -0800

    First commit, application works but I would like to improve the UI.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Input from './Components/input';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Spelling Bee </h1>
+      <div>
+        Enter the letters for the Spelling Bee you would like to solve. Enter each letter with a space between them. Enter the primary letter last.
+      </div>
+      
+      <div className="text-input-container"> {/* Apply CSS class to the text input container */}
+        <Input/>
+      </div>
     </div>
   );
 }
